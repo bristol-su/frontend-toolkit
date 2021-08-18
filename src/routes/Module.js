d4fa68ded851160af1d7ece9bb285119c6b0b01b
Toby Twigger <tobytwigger1@gmail.com>
2021-08-18 10:53:07 +0100

    Change APP url to api url

--- src/routes/Module.js
+++ src/routes/Module.js
@@ -3,9 +3,9 @@ import WindowAccessor from '../utils/WindowAccessor';
 
 export default new class {
     moduleUrl() {
-        if(WindowAccessor.has('APP_URL') && environment.activity.has() && environment.moduleInstance.has()) {
+        if(WindowAccessor.has('API_URL') && environment.activity.has() && environment.moduleInstance.has()) {
             return [
-              WindowAccessor.get('APP_URL'),
+              WindowAccessor.get('API_URL'),
                 (environment.authentication.admin() ? 'a' : 'p'),
                 environment.activity.get().slug,
                 environment.moduleInstance.get().slug,
